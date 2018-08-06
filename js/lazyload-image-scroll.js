bccfc60d943b7f8debef8255113d960ee9f17a38
torresgol10 <torresgol10.it@gmail.com>
2018-08-06 22:33:52 +0200

    cambio las declaracion let por var, a google no le gusta la declaracion let por que no es totalmente complatible con todos los navegadores

--- js/lazyload-image-scroll.js
+++ js/lazyload-image-scroll.js
@@ -1,23 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-  let active = false;
+  var lazyImagesScroll = [].slice.call(document.querySelectorAll("img.lazy"));
+  var activeLazyImage = false;
 
   const lazyLoad = function() {
-    if (active === false) {
-      active = true;
+    if (activeLazyImage === false) {
+      activeLazyImage = true;
 
       setTimeout(function() {
-        lazyImages.forEach(function(lazyImage) {
+        lazyImagesScroll.forEach(function(lazyImage) {
           if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
             lazyImage.src = lazyImage.dataset.src;
             lazyImage.srcset = lazyImage.dataset.srcset;
+
             lazyImage.classList.remove("lazy");
 
-            lazyImages = lazyImages.filter(function(image) {
+            lazyImagesScroll = lazyImagesScroll.filter(function(image) {
               return image !== lazyImage;
             });
 
-            if (lazyImages.length === 0) {
+            if (lazyImagesScroll.length === 0) {
               document.removeEventListener("scroll", lazyLoad);
               window.removeEventListener("resize", lazyLoad);
               window.removeEventListener("orientationchange", lazyLoad);
@@ -25,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
           }
         });
 
-        active = false;
+        activeLazyImage = false;
       }, 200);
     }
   };
